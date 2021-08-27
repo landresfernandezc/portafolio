f7e39f3d8a87d478d9c9809decd6e91e67520923
Luis Andrés <landresf3638@gmail.com>
2021-08-26 20:22:39 -0600

    MyPortafolio

---
+++ public/project.js
@@ -0,0 +1,12 @@
+let projects=[
+    {
+        name:"GreenBill",
+        tags:"#javascript,#fullstack,#backbone,#ui,#backend,#jquery,#grails,#sql",
+        image:"GreenBill.png"
+    },
+    {
+        name:"ERP",
+        tags:"#javascript,#fullstack,#ui,#backend,#jquery,#coldfusion,#sql",
+        image:"ERP.png"
+    }
+]
---
+++ server.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const path= require('path');
+const nodemailer=require('nodemailer');
+const dotenv=require('dotenv');
+dotenv.config()
+let initialPath=path.join(__dirname,"public");
+let app=express();
+app.use(express.static(initialPath));
+app.use(express.json())
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(initialPath,"index.html"));
+})
+app.post('/mail',(req,res)=>{
+    const {firsname,lastname,email,msg}=req.body;
+    const transporter=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:process.env.EMAIL,
+            pass:process.env.PASSWORD
+        }
+    })
+    const mailOptions={
+        from:'portafolio2021.01@gmail.com',
+        to:'portafolio2021.01@gmail.com',
+        subject:'Portafolio notification',
+        text:`First name: ${firsname},  \n Last name:${lastname} ,\n Email:${email} ,\n Message:${msg} `
+    }
+    transporter.sendMail(mailOptions,(err,result)=>{
+        if(err){
+            console.log(err)
+            res.json('opps! it seems like some error ocurred please. Try Again')
+        }else{
+            res.json('Thanks for e-mailing me. I will reply to you within 48 hours')
+        }
+    })
+})
+
+app.listen('3000',()=>{
+    console.log("listening")
+})
